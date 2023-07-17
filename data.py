75fd28b6f7f038281fafc107936988d6fc869d93
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2023-07-17 13:57:32 +0900

    fix: mini_batch_size 파라미터 사용하도록 수정

--- data.py
+++ data.py
@@ -2,5 +2,5 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
 def load_data_using_mini_batch(dataset: Dataset,mini_batch_size: int) -> DataLoader:
-    data_loader = DataLoader(dataset,batch_size=2,shuffle=True)
+    data_loader = DataLoader(dataset,batch_size=mini_batch_size,shuffle=True)
     return data_loader
