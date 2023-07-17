11f46d084fb41fcf82e187ab92b400fb60ce3f2f
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2023-07-17 15:34:59 +0900

    fix: 데이터셋 선언 부분 수정

--- dataset.py
+++ dataset.py
@@ -4,8 +4,8 @@ from torch import Tensor
 
 class CustomDataset(Dataset):
     def __init__(self):
-        self.x_data = [[1,2,3],[4,5,6],[7,8,9]]
-        self.y_data = [[12],[18],[11]]
+        self.x_data = [[x * i for x in [1,2,3]] for i in range(1,5)]
+        self.y_data = [[12],[18],[11],[14]]
     def __len__(self):
         return len(self.x_data)
     def __getitem__(self,idx):
