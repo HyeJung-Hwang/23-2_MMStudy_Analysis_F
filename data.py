10d71aab64b37a3c17857886cd2e406b768fcd67
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2023-07-17 13:55:53 +0900

    feat: mini batch 경사하강법 활용해서 data load하는 모듈

---
+++ data.py
@@ -0,0 +1,6 @@
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+
+def load_data_using_mini_batch(dataset: Dataset,mini_batch_size: int) -> DataLoader:
+    data_loader = DataLoader(dataset,batch_size=2,shuffle=True)
+    return data_loader
