fe5768849aaa9105c67043090287996c0b19ddf5
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2023-07-18 00:31:29 +0900

    fix: 모델저장할 경로 파라미터 추가

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ def train_model(
     criterion: nn.Module,
     optimizer: Optimizer,
     device: str,
+    path_to_save_model: str,
     num_epochs: int = 13,
     is_train: bool = True,
 ) -> Tuple[List[float], List[float]]:
@@ -55,7 +56,7 @@ def train_model(
         loss_history.append(epoch_loss)
         torch.save(
             model.state_dict(),
-            os.path.join("../chap05/data/catanddog/", "{0:0=2d}.pth".format(epoch)),
+            os.path.join(path_to_save_model, "{0:0=2d}.pth".format(epoch)),
         )
         print()
 
