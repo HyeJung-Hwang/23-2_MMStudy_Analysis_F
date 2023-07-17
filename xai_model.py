89f5794c16a48d5a2ef12316da478248af6567ac
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2023-07-18 01:01:23 +0900

    feat: XAI model

---
+++ xai_model.py
@@ -0,0 +1,82 @@
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+
+class XAI(torch.nn.Module):
+    def __init__(self, num_classes=2):
+        super(XAI, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+            nn.Conv2d(64, 64, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(128, 128, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(256, 256, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(256, 256, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 512, bias=False),
+            nn.Dropout(0.5),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(-1, 512)
+        x = self.classifier(x)
+        return F.log_softmax(x)
