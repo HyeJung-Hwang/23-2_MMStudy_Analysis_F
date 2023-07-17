d210937728b816e05c995ca62cbb5e724e694179
황혜정 <hwanghyejeong@hwanghyejeong-ui-MacBookPro.local>
2023-07-17 19:13:54 +0900

    feat: FashionDNN 모델

--- models.py
+++ models.py
@@ -15,3 +15,19 @@ class DropoutModel(torch.nn.Module):
         x = F.relu(self.layer2(x))
         x = self.dropout2(x)
         return self.layer3(x)
+
+class FashionDNN(torch.nn.Module):
+    def __init__(self):
+        super(FashionDNN,self).__init__()
+        self.fc1 = torch.nn.Linear(in_features=784,out_features=256)
+        self.drop = torch.nn.Dropout(0.25)
+        self.fc2 = torch.nn.Linear(in_features=256,out_features=128)
+        self.fc3 = torch.nn.Linear(in_features=128,out_features=10)
+
+    def forward(self,input_data):
+        out = input_data.view(-1,784)
+        out = F.relu(self.fc1(out))
+        out = self.drop(out)
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
