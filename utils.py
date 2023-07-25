2c246da4079337eb2d09322233aaff2cef06f0d9
Hyejung Hwang <seathat33@gmail.com>
2023-07-25 11:49:41 +0900

    feat: calculate epoch time

---
+++ utils.py
@@ -0,0 +1,7 @@
+from typing import Tuple
+
+def epoch_time(start_time, end_time) -> Tuple[int, int]:
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
