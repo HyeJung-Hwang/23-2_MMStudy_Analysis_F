900a24215c36dacdd821f4152a03e3ef479d4bd6
Hyejung Hwang <seathat33@gmail.com>
2023-07-25 11:48:09 +0900

    feat: evaluate

--- test.py
+++ test.py
@@ -34,3 +34,19 @@ def calculate_accuracy(y_pred: torch.Tensor, y:torch.Tensor) -> torch.Tensor:
     correct = top_pred.eq(y.view_as(top_pred)).sum()
     acc = correct.float() / y.shape[0]
     return acc
+
+def evaluate(model, iterator, criterion, device):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in iterator:
+            x = x.to(device)
+            y = y.to(device)
+            y_pred, _ = model(x)
+            loss = criterion(y_pred, y)
+            acc = calculate_accuracy(y_pred, y)
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
