6dbf8b8be09f70794546b97b5f36333c403d619e
Hyejung Hwang <seathat33@gmail.com>
2023-08-09 15:25:25 +0900

    feat: TextCNN에 batch normalization layer 추가

--- models/text_cnn.py
+++ models/text_cnn.py
@@ -6,7 +6,8 @@ class TextCNN(nn.Module):
     def __init__(
             self,
             num_filters: int,filter_sizes: list, vocab_size: int,
-            embedding_size: int,sequence_length: int,num_classes: int
+            embedding_size: int,sequence_length: int,num_classes: int,
+            is_batch_normalize: bool = False,
         ):
         super(TextCNN, self).__init__()
         self.filter_sizes = filter_sizes
@@ -16,22 +17,25 @@ class TextCNN(nn.Module):
         self.Weight = nn.Linear(self.num_filters_total, num_classes, bias=False)
         self.Bias = nn.Parameter(torch.ones([num_classes]))
         self.filter_list = nn.ModuleList([nn.Conv2d(1, num_filters, (size, embedding_size)) for size in filter_sizes])
+        self.batch_norm_list = nn.ModuleList([nn.BatchNorm2d(num_filters) for _ in filter_sizes]) if is_batch_normalize else None
 
     def forward(self, X):
-        embedded_chars = self.W(X) # [batch_size, sequence_length, sequence_length]
-        embedded_chars = embedded_chars.unsqueeze(1) # add channel(=1) [batch, channel(=1), sequence_length, embedding_size]
+        embedded_chars = self.W(X) 
+        embedded_chars = embedded_chars.unsqueeze(1) 
 
         pooled_outputs = []
         for i, conv in enumerate(self.filter_list):
-            # conv : [input_channel(=1), output_channel(=3), (filter_height, filter_width), bias_option]
             h = F.relu(conv(embedded_chars))
-            # mp : ((filter_height, filter_width))
+            
+            if self.batch_norm_list is not None:
+                batch_norm = self.batch_norm_list[i]
+                h = batch_norm(h)
+            
             mp = nn.MaxPool2d((self.sequence_length - self.filter_sizes[i] + 1, 1))
-            # pooled : [batch_size(=6), output_height(=1), output_width(=1), output_channel(=3)]
             pooled = mp(h).permute(0, 3, 2, 1)
             pooled_outputs.append(pooled)
 
-        h_pool = torch.cat(pooled_outputs, len(self.filter_sizes)) # [batch_size(=6), output_height(=1), output_width(=1), output_channel(=3) * 3]
-        h_pool_flat = torch.reshape(h_pool, [-1, self.num_filters_total]) # [batch_size(=6), output_height * output_width * (output_channel * 3)]
-        model = self.Weight(h_pool_flat) + self.Bias # [batch_size, num_classes]
+        h_pool = torch.cat(pooled_outputs, len(self.filter_sizes)) 
+        h_pool_flat = torch.reshape(h_pool, [-1, self.num_filters_total]) 
+        model = self.Weight(h_pool_flat) + self.Bias 
         return model
